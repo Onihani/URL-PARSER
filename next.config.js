9f8f305193abad7f7ebc5c5bfb201835dd010b39
Nathaniel Quansah <n.bongo40@gmail.com>
2021-10-04 21:43:36 +0000

    removed next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // reactStrictMode: true,
-  env: {
-    DATABASE_URL: "postgres://lcjmzskpxjgrcu:ece79823490bf05faafbe0506edc6a1ab72545d149e264bbcfbb60933c234807@ec2-44-198-154-255.compute-1.amazonaws.com:5432/dcna7c8sgheq0j"
-  },
-};
